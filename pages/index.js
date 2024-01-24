f106da8a2e05e9f04aa20a1383503cc0e99fb18c
Connie Ooi <connie.ooi18@gmail.com>
2024-01-23 16:03:27 -0800

    added some files and now adding mongodb

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,24 @@
 import Image from "next/image";
 import {Inter} from "next/font/google";
+import {useSession, signIn, signOut} from "next-auth/react";
 
 export default function Home() {
-  return <div className={"bg-red-500"}>test</div>;
+  const {data: session} = useSession();
+
+  if (!session) {
+    return (
+      <div className={"bg-blue-900 w-screen h-screen flex items-center"}>
+        <div className="text-center w-full">
+          <button
+            className="bg-white p-2 px-4 rounded-lg"
+            onClick={() => signIn("google")}
+          >
+            Login with Google
+          </button>
+        </div>
+      </div>
+    );
+  }
+
+  return <div>logged in {session.user.email}</div>;
 }
