7660463b67798ecc2d6326448d30ce2eb1705c6c
Connie Ooi <connie.ooi18@gmail.com>
2024-01-24 21:35:31 -0800

    created products

--- pages/index.js
+++ pages/index.js
@@ -2,31 +2,14 @@ import Image from "next/image";
 import {Inter} from "next/font/google";
 import {useSession, signIn, signOut} from "next-auth/react";
 import Nav from "@/components/Nav";
+import Layout from "@/components/Layout";
 
 export default function Home() {
-  const {data: session} = useSession();
-
-  if (!session) {
-    return (
-      <div className={"bg-blue-900 w-screen h-screen flex items-center"}>
-        <div className="text-center w-full">
-          <button
-            className="bg-white p-2 px-4 rounded-lg"
-            onClick={() => signIn("google")}
-          >
-            Login with Google
-          </button>
-        </div>
-      </div>
-    );
-  }
-
+  const {data: session} = useSession(); // renaming data to session
+  if (!session) return;
   return (
-    <div className="bg-blue-900 min-h-screen flex">
-      <Nav />
-      <div className="bg-white flex-grow mt-2 mr-2 mb-2 rounded-lg p-4">
-        logged in {session.user.email}
-      </div>
-    </div>
+    <Layout>
+      <div className="text-blue-900">Hello, {session?.user?.email}</div>
+    </Layout>
   );
 }
