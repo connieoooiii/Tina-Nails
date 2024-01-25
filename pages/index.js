23d216db5779bc5fd0d37c2bb8a1afb3f13761e7
Connie Ooi <connie.ooi18@gmail.com>
2024-01-24 21:43:46 -0800

    dashboard basics

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,19 @@ export default function Home() {
   if (!session) return;
   return (
     <Layout>
-      <div className="text-blue-900">Hello, {session?.user?.email}</div>
+      <div className="text-blue-900 flex justify-between">
+        <h2>
+          Hello, <b>{session?.user?.name}</b>
+        </h2>
+        <div className="flex bg-gray-300 text-black gap-1 rounded-lg overflow-hidden">
+          <img
+            src={session?.user?.image}
+            alt="session user image"
+            className="w-6 h-6 "
+          />
+          <span className="px-2">{session?.user?.name}</span>
+        </div>
+      </div>
     </Layout>
   );
 }
