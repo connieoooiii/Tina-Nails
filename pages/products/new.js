6a8fc950dedb88dfd95fddc1db579227752702a3
Connie Ooi <connie.ooi18@gmail.com>
2024-01-25 17:55:06 -0800

    new product form

--- pages/products/new.js
+++ pages/products/new.js
@@ -4,9 +4,13 @@ export default function NewProduct() {
   return (
     <Layout>
       <h1>New Product</h1>
+      <label>Product Name</label>
       <input type="text" placeholder="product name" />
+      <label>Description</label>
       <textarea type="text" placeholder="description" />
-      <input type="text" placeholder="price" />
+      <label>Price (in USD)</label>
+      <input type="number" placeholder="price" />
+      <button className="btn-primary">Save</button>
     </Layout>
   );
 }
