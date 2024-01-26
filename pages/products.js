cd5c5a89e434259dbc8978a2cab4300bcd5a6cba
Connie Ooi <connie.ooi18@gmail.com>
2024-01-25 17:43:24 -0800

    new product

--- pages/products.js
+++ pages/products.js
@@ -1,5 +1,15 @@
 import Layout from "@/components/Layout";
+import Link from "next/link";
 
 export default function products() {
-  return <Layout>products page here</Layout>;
+  return (
+    <Layout>
+      <Link
+        href={"/products/new"}
+        className="bg-blue-900 rounded-md text-white py-1 px-2"
+      >
+        Add new product
+      </Link>
+    </Layout>
+  );
 }
