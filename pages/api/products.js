0b4c562105b1cb5873b793ef308b5a57ea902035
Connie Ooi <connie.ooi18@gmail.com>
2024-01-30 20:22:43 -0800

    mongoose connection

--- pages/api/products.js
+++ pages/api/products.js
@@ -1,17 +1,17 @@
-import clientPromise from "@/lib/mongodb";
+import {mongooseConnect} from "@/lib/mongoose";
 import {Product} from "@/models/Product";
 import mongoose from "mongoose";
 
 export default async function handle(req, res) {
   const {method} = req;
-  mongoose.Promise = clientPromise;
+  await mongooseConnect();
   if (method === "POST") {
     const {title, description, price} = req.body;
-    await Product.create({
+    const productDoc = await Product.create({
       title,
       description,
       price,
     });
-    res.json("post");
+    res.json(productDoc);
   }
 }
