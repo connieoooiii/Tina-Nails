b0ac7f52ec8613c97ad8d1ef0e22874b3627183d
Connie Ooi <connie.ooi18@gmail.com>
2024-01-31 17:45:54 -0800

    created prdouct table

--- pages/products.js
+++ pages/products.js
@@ -1,12 +1,14 @@
 import Layout from "@/components/Layout";
 import axios from "axios";
 import Link from "next/link";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 
 export default function Products() {
+  const [products, setProducts] = useState([]);
+
   useEffect(() => {
     axios.get("/api/products").then((res) => {
-      console.log(res.data);
+      setProducts(res.data);
     });
   }, []);
   return (
@@ -17,6 +19,24 @@ export default function Products() {
       >
         Add new product
       </Link>
+      <table className="basic mt-2">
+        <thead>
+          <tr>
+            <td>Product name</td>
+            <td></td>
+          </tr>
+        </thead>
+        <tbody>
+          {products.map((product) => (
+            <tr>
+              <td>{product.title}</td>
+              <td>
+                <Link href={"/products/" + product._id}>Edit</Link>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </Layout>
   );
 }
