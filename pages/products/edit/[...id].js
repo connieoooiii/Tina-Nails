81cd38eef8156b13105a37e1ee7435051f2a12d1
Connie Ooi <connie.ooi18@gmail.com>
2024-02-01 20:50:49 -0800

    find one product

--- pages/products/edit/[...id].js
+++ pages/products/edit/[...id].js
@@ -1,9 +1,17 @@
 import Layout from "@/components/Layout";
+import axios from "axios";
 import {useRouter} from "next/router";
+import {useEffect} from "react";
 
 export default function EditProductPage() {
   const router = useRouter();
+  const {id} = router.query;
 
+  useEffect(() => {
+    axios.get("/api/products?id=" + id).then((res) => {
+      console.log("res", res.data);
+    });
+  }, [id]);
   return (
     <Layout>
       <div></div>
