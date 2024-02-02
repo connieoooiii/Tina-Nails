c7014aafeb84a45f20dca764cc9bbd29da0a0532
Connie Ooi <connie.ooi18@gmail.com>
2024-02-01 20:40:25 -0800

    added edit page

---
+++ pages/products/edit/[...editproduct].js
@@ -0,0 +1,13 @@
+import Layout from "@/components/Layout";
+import {useRouter} from "next/router";
+
+export default function EditProductPage() {
+  const router = useRouter();
+
+  return (
+    <Layout>
+      <div></div>
+      edit product form
+    </Layout>
+  );
+}
