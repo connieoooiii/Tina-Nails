81c8e44d5e6912d4ee8a43d734e4fab309cae42a
Connie Ooi <connie.ooi18@gmail.com>
2024-02-05 20:46:03 -0800

    product form component

--- pages/products/edit/[...id].js
+++ pages/products/edit/[...id].js
@@ -8,6 +8,7 @@ export default function EditProductPage() {
   const {id} = router.query;
 
   useEffect(() => {
+    if (!id) return;
     axios.get("/api/products?id=" + id).then((res) => {
       console.log("res", res.data);
     });
--- pages/products/new.js
+++ pages/products/new.js
@@ -1,54 +1,10 @@
 import Layout from "@/components/Layout";
-import axios from "axios";
-import {useRouter} from "next/router";
-import {useState} from "react";
-import {useEffect} from "react";
+import ProductFrom from "@/components/ProductForm";
 
 export default function NewProduct() {
-  const [title, setTitle] = useState("");
-  const [description, setDescription] = useState("");
-  const [price, setPrice] = useState("");
-  const [goToProducts, setGoToProducts] = useState(false);
-  const router = useRouter();
-  async function createProduct(e) {
-    e.preventDefault();
-    const data = {title, description, price};
-    await axios.post("/api/products", data);
-    setGoToProducts(true);
-  }
-
-  if (goToProducts) {
-    router.push("/products");
-  }
   return (
     <Layout>
-      <form onSubmit={createProduct}>
-        <h1>New Product</h1>
-        <label>Product Name</label>
-        <input
-          type="text"
-          placeholder="product name"
-          value={title}
-          onChange={(ev) => setTitle(ev.target.value)}
-        />
-        <label>Description</label>
-        <textarea
-          type="text"
-          placeholder="description"
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
-        />
-        <label>Price (in USD)</label>
-        <input
-          type="number"
-          placeholder="price"
-          value={price}
-          onChange={(e) => setPrice(e.target.value)}
-        />
-        <button type="submit" className="btn-primary">
-          Save
-        </button>
-      </form>
+      <ProductFrom />
     </Layout>
   );
 }
