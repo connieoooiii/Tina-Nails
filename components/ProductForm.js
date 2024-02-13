32ffe44ed3c517a8116ff063af79cc62a4b52abe
Connie Ooi <connie.ooi18@gmail.com>
2024-02-12 22:01:50 -0800

    created photo on form

--- components/ProductForm.js
+++ components/ProductForm.js
@@ -9,6 +9,7 @@ export default function ProductForm({
   title: existingTitle,
   description: existingDescription,
   price: existingPrice,
+  images,
 }) {
   const [title, setTitle] = useState(existingTitle || "");
   const [description, setDescription] = useState(existingDescription || "");
@@ -19,8 +20,10 @@ export default function ProductForm({
     e.preventDefault();
     const data = {title, description, price};
     if (_id) {
+      //update
       await axios.put("/api/products", {...data, _id});
     } else {
+      //create
       await axios.post("/api/products", data);
     }
     setGoToProducts(true);
@@ -38,6 +41,27 @@ export default function ProductForm({
         value={title}
         onChange={(ev) => setTitle(ev.target.value)}
       />
+      <label>Photos</label>
+      <div className="mb-2">
+        <button className="w-24 h-24  text-center flex  items-center justify-center text-sm gap-1 text-gray-500 rounded-lg bg-gray-200">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5m-13.5-9L12 3m0 0 4.5 4.5M12 3v13.5"
+            />
+          </svg>
+          <div>Upload</div>
+        </button>
+        {!images?.length && <div>No photos in this products</div>}
+      </div>
       <label>Description</label>
       <textarea
         type="text"
