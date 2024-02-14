7cb115bd3aba6663e18e24a387b6e860f9826a3f
Connie Ooi <connie.ooi18@gmail.com>
2024-02-13 21:53:52 -0800

    promise

--- pages/api/upload.js
+++ pages/api/upload.js
@@ -2,10 +2,15 @@ import multiparty from "multiparty";
 
 export default async function handle(req, res) {
   const form = new multiparty.Form();
-  form.parse(req, (err, fields, files) => {
-    console.log("filelenght", files.length);
-    res.json("ok");
+  const {fields, files} = new Promise((resolve, reject) => {
+    form.parse(req, (err, fields, files) => {
+      if (err) reject(err);
+      resolve({fields, files});
+    });
   });
+  console.log("filelenght", files.length);
+  console.log("fields", fields);
+  return res.json("ok");
 }
 
 export const config = {
