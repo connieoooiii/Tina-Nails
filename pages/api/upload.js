5bde1211b8c050277559f02d5fafb25f6f8e3f46
Connie Ooi <connie.ooi18@gmail.com>
2024-02-14 21:26:37 -0800

    upload to s3

--- pages/api/upload.js
+++ pages/api/upload.js
@@ -1,4 +1,7 @@
 import multiparty from "multiparty";
+import {PutObjectCommand, S3Client} from "@aws-sdk/client-s3";
+import fs from "fs";
+const bucketName = "tina-nails";
 
 export default async function handle(req, res) {
   const form = new multiparty.Form();
@@ -8,7 +11,29 @@ export default async function handle(req, res) {
       resolve({fields, files});
     });
   });
-  console.log("filelenght", files.file.length);
+  console.log("filelength", files.file.length);
+  const client = new S3Client({
+    region: "us-west-1",
+    credentials: {
+      accessKeyId: process.env.S3_ACCESS_KEY,
+      secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+    },
+  });
+
+  for (const file of files.file) {
+    const ext = file.originalFilename.split(".").pop();
+    const newFilename = Date.now() + "." + ext;
+    console.log(newFilename);
+    await client.send(
+      new PutObjectCommand({
+        Bucket: bucketName,
+        Key: newFilename,
+        Body: fs.readFileSync(file.path),
+        ACL: "public-read",
+        ContentType: "",
+      })
+    );
+  }
 
   return res.json("ok");
 }
